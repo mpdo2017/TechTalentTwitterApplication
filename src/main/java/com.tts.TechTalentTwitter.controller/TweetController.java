5e94b9a71475c7466427525303a62ab7c58fa623
Michele <mpdo2017@gmail.com>
2019-09-29 17:10:58 -0400

    Added feed paga filtering function

--- src/main/java/com.tts.TechTalentTwitter.controller/TweetController.java
+++ src/main/java/com.tts.TechTalentTwitter.controller/TweetController.java
@@ -1,6 +1,7 @@
 package com.tts.TechTalentTwitter.controller;
 
 import com.tts.TechTalentTwitter.model.Tweet;
+import com.tts.TechTalentTwitter.model.TweetDisplay;
 import com.tts.TechTalentTwitter.model.User;
 import com.tts.TechTalentTwitter.service.TweetService;
 import com.tts.TechTalentTwitter.service.UserService;
@@ -8,12 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,10 +29,23 @@ public class TweetController {
 
 
     @GetMapping({"/tweets", "/"})
-    public String getFeed(Model model) {
-        List<TweetDisplay> tweets = tweetService.findAll();
-        model.addAttribute("tweetList", tweets);
-        return "feed";
+    public String getFeed(@RequestParam(value = "filter", required = false)
+                                  String filter, Model model) {
+        User loggedInUser = userService.getLoggedInUser();
+        List<TweetDisplay> tweets = new ArrayList<>();
+        if (filter == null) {
+            filter = "all";
+            if (filter.equalsIgnoreCase("following")) {
+                List<User> following = loggedInUser.getFollowing();
+                tweets = tweetService.findAllByUsers(following);
+                model.addAttribute("filter", "following");
+            } else {
+                tweets = tweetService.findAll();
+                model.addAttribute("filter", "all");
+            }
+            model.addAttribute("tweetList", tweets);
+            return "feed";
+        }
     }
 
     @GetMapping("/tweets/new")
