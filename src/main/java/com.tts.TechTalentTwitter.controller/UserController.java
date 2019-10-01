1eb00824a4a6bbc8609102741d9467a587de3a56
Michele <mpdo2017@gmail.com>
2019-10-01 12:39:18 -0400

    Added hashtag, following/unfollowing with javascript functionality

--- src/main/java/com.tts.TechTalentTwitter.controller/UserController.java
+++ src/main/java/com.tts.TechTalentTwitter.controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.List;
 
